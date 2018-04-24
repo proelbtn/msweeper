31b8dcbb8a6280eb9f9bc8433b4348ab360b6ab4
proelbtn <proelbtn@users.noreply.github.com>
2018-04-24 17:23:09 +0900

    [ADD] クリックしてそれっぽく見える部分までは作った。

--- javascript/src/App.vue
+++ javascript/src/App.vue
@@ -1,28 +1,72 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
-  </div>
+  <svg id="app" width="240" height="240">
+    <g v-for="y in range(8)">
+      <g v-for="x in range(8)">
+        <Cell :x="x"
+              :y="y"
+              :state="getState(x, y)"
+              :opened="opened[y * 8 + x]"
+              @open="open"
+        />
+      </g>
+    </g>
+  </svg>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+  import Cell from './components/Cell';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
-};
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1)), t;
+      t = array[i];
+      array[i] = array[j];
+      array[j] = t;
+    }
+    return array;
+  }
+
+  export default {
+    name: 'App',
+    components: {
+      Cell
+    },
+    methods: {
+      range(n) {
+        let t = [];
+        for (let i = 0; i < n; i++) t[i] = i;
+        return t;
+      },
+      getState(x, y) {
+        let count = 0;
+        for (let dy = -1; dy < 2; dy++) {
+          for (let dx = -1; dx < 2; dx++) {
+            if (!dx && !dy) continue;
+            const nx = x + dx, ny = y + dy;
+            if (0 <= nx && nx < 8 && 0 <= ny && ny < 8 && this.bomb[ny * 8 + nx]) count++;
+          }
+        }
+      },
+      open(x, y) {
+        this.opened.splice(y * 8 + x, 1, true);
+      }
+    },
+    data() {
+      let bomb = [], opened = [];
+      for (let pos = 0; pos < 64; pos++) {
+        opened[pos] = false;
+        bomb[pos] = pos < 10;
+      }
+      return {
+        bomb: shuffleArray(bomb),
+        opened: opened
+      }
+    }
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    background: #BDBDBD;
+  }
 </style>
---
+++ javascript/src/components/Cell.vue
@@ -0,0 +1,32 @@
+<template>
+  <g>
+    <rect class="cell" :class="{ concealed: !opened }"
+          :x="x * 30 + 1"
+          :y="y * 30 + 1"
+          width="28"
+          height="28"
+          @click="$emit('open', x, y)">
+    </rect>
+  </g>
+</template>
+
+<script>
+  export default {
+    name: 'Cell',
+    props: ['x', 'y', 'state', 'opened']
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .cell {
+    fill-opacity: 0.0;
+    stroke: #757575;
+    stroke-width: 1px;
+  }
+
+  .concealed {
+    fill: #EEEEEE;
+    fill-opacity: 1.0;
+  }
+</style>
