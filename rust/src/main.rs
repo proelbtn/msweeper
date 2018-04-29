014164c272503b90732ee5963c3df06d9cc91fd0
proelbtn <proelbtn@users.noreply.github.com>
2018-04-29 23:21:49 +0900

    [ADD] i32の変数を受け取った。

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,13 @@
+use std::io::{stdout, stdin, Write};
+
+fn get_integer(name: &str) -> i32 {
+    let mut s = String::new();
+    print!("{} : ", name);
+    let _ = stdout().flush();
+    stdin().read_line(&mut s).expect("ERR : Read");
+    s.trim().parse::<i32>().expect("ERR : Cast")
+}
+
 fn main() {
-    println!("Hello, world!");
+    let (width, height, bnum) = (get_integer("width"), get_integer("height"), get_integer("bnum"));
 }
