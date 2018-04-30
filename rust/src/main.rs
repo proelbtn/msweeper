5906fac1c3ada3f075700b0f0252a31823c5b41c
proelbtn <proelbtn@users.noreply.github.com>
2018-04-30 15:45:35 +0900

    [ADD] ゲームとして動く部分まで作った。

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -6,11 +6,16 @@ use std::io::{stdout, stdin, Write};
 use msweeper::Msweeper;
 
 fn get_input<T>(name: &str) -> T where T: std::str::FromStr, <T as std::str::FromStr>::Err: std::fmt::Debug {
-    let mut s = String::new();
-    print!("{} : ", name);
-    let _ = stdout().flush();
-    stdin().read_line(&mut s).expect("ERR : Read");
-    s.trim().parse::<T>().expect("ERR : Cast")
+    loop {
+        let mut s = String::new();
+        print!("{} : ", name);
+        let _ = stdout().flush();
+        stdin().read_line(&mut s).expect("unknown error occured.");
+        match s.trim().parse::<T>() {
+            Ok(v) => return v,
+            Err(_) => println!("Please input correctly")
+        }
+    }
 }
 
 fn display_map(ms: &Msweeper) {
@@ -18,7 +23,13 @@ fn display_map(ms: &Msweeper) {
     let h = ms.get_height();
     for y in 0..h {
         for x in 0..w {
-            print!("{}", ms.at(x, y));
+            let (opened, num) = match ms.at(x, y) {
+                Ok(val) => val,
+                Err(_) => panic!("ERR : ms.at returned enexpected value.")
+            };
+            if !opened { print!("."); }
+            else if num == 0 { print!(" "); }
+            else { print!("{}", num); }
         }
         print!("\n");
     }
@@ -29,7 +40,16 @@ fn main() {
     let height: usize = get_input("height");
     let bnum: usize = get_input("bnum");
     
-    let ms = Msweeper::new(width, height, bnum);
+    let mut ms = Msweeper::new(width, height, bnum);
 
-    display_map(&ms);
+    while !ms.is_gameend() {
+        display_map(&ms);
+        let x: usize = get_input("X");
+        let y: usize = get_input("Y");
+
+        match ms.open(x, y) {
+            Ok(_) => (),
+            Err(_) => println!("width or height is not correct.")
+        }
+    }
 }
--- rust/src/msweeper.rs
+++ rust/src/msweeper.rs
@@ -29,19 +29,67 @@ impl Msweeper {
     }
 
     pub fn get_height(&self) -> usize {
-        self.width
+        self.height
     }
 
-    pub fn at(&self, x: usize, y: usize) -> i32 {
-        let c = 0;
+    pub fn at(&self, x: usize, y: usize) -> Result<(bool, i32), String> {
+        if !(x < self.width && y < self.height) { 
+            return Err("index out of bounds.".to_string()); 
+        }
+
+        let mut c = 0;
         for dy in -1..2 {
             for dx in -1..2 {
-                if x == 0 && dx == -1 { continue }
-                if y == 0 && dy == -1 { continue }
-                if x + 1 == self.width && dy == 1 { continue }
-                if y + 1 == self.height && dy == 1 { continue }
+                if !self.b_check(x, y, dx, dy) { continue }
+                let pos = (y as i32 + dy) as usize * self.width + (x as i32 + dx) as usize;
+                if self.map[pos].1 { c = c + 1; }
             }
         }
-        if self.map[y * self.width + x].0 { c } else { -1 }
+        Ok((self.map[y * self.width + x].0, c))
+    }
+
+    pub fn is_gameend(&self) -> bool {
+        let mut end = true;
+        for i in 0..(self.width * self.height) {
+            let (opened, bombed) = self.map[i];
+            if opened && bombed { break; }
+            if !opened && !bombed { end = false; break; }
+        }
+        end
+    }
+
+    pub fn open(&mut self, x: usize, y: usize) -> Result<(), String> {
+        if !(x < self.width && y < self.height) { 
+            return Err("index out of bounds.".to_string()); 
+        }
+
+        self.map[y * self.width + x].0 = true;
+
+        let num = match self.at(x, y) {
+            Ok((_, num)) => num,
+            Err(_) => return Err("internal processing error.".to_string())
+        };
+
+        if num == 0 {
+            for dy in -1..2 {
+                for dx in -1..2 {
+                    if !self.b_check(x, y, dx, dy) { continue }
+                    let (cx, cy) = ((x as i32 + dx) as usize, (y as i32 + dy) as usize);
+                    match self.at(cx, cy) {
+                        Ok((opened, _)) => if !opened { self.open(cx, cy); },
+                        Err(_) => return Err("internal processing error.".to_string())
+                    }
+                }
+            }
+        }
+        Ok(())
+    }
+
+    fn b_check(&self, x: usize, y: usize, dx: i32, dy: i32) -> bool {
+        !(dx == 0 && dy == 0) 
+            && !(x ==0 && dx == -1) 
+            && !(y == 0 && dy == -1) 
+            && !(x + 1 == self.width && dx == 1) 
+            && !(y + 1 == self.height && dy == 1)
     }
 }
