457ac09322514bff4500422c62e178e44d093087
proelbtn <proelbtn@users.noreply.github.com>
2018-05-02 14:56:39 +0900

    [ADD] ゲームとして遊べるようになった。

--- javascript/src/App.vue
+++ javascript/src/App.vue
@@ -1,20 +1,51 @@
 <template>
-  <svg id="app" :width="width * 30" :height="height * 30">
-    <g v-for="y in range(height)">
-      <g v-for="x in range(width)">
+  <svg id="app" :width="w * 30" :height="h * 30">
+    <g v-for="y in range(h)">
+      <g v-for="x in range(w)">
         <Cell :x="x"
               :y="y"
-              :state="getState(x, y)"
-              :opened="opened[y * width + x]"
+              :state="cells[y * w + x]"
+              :opened="opened[y * w + x]"
               @open="open"
         />
       </g>
     </g>
+    <rect v-if="getGameState() != 0" class="vail-rect"
+          x="0"
+          y="0"
+          :width="w * 30"
+          :height="h * 30" />
+    <text class="result result-clear" v-if="getGameState() == 1"
+          :x="w * 30 / 2" 
+          :y="h * 30 / 2" 
+          text-anchor="middle" 
+          dominant-baseline="central">
+      GAME CLEAR!
+    </text>
+    <text class="result result-over" v-if="getGameState() == 2"
+          :x="w * 30 / 2" 
+          :y="h * 30 / 2" 
+          text-anchor="middle" 
+          dominant-baseline="central">
+      GAME OVER...
+    </text>
+    <text class="result result-error" v-if="getGameState() == 3"
+          :x="w * 30 / 2" 
+          :y="h * 30 / 2" 
+          text-anchor="middle" 
+          dominant-baseline="central">
+      ERROR!
+    </text>
   </svg>
 </template>
 
 <script>
   import Cell from './components/Cell';
+
+  const GAME_IS_STILL_CONTINUED = 0;
+  const GAME_IS_CLEAR = 1;
+  const GAME_IS_OVER = 2;
+  const GAME_IS_ERROR = 3;
 
   function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
@@ -30,9 +61,13 @@
     return 0 <= x && x < w && 0 <= y && y < h;
   }
 
+  function coord2pos(x, y, w, h) {
+    return y * w + x;
+  }
+
   export default {
     name: 'App',
-    props: ['width', 'height', 'bnum'],
+    props: ['w', 'h', 'n'],
     components: {
       Cell
     },
@@ -43,45 +78,70 @@
         return t;
       },
 
-      getState(x, y) {
-        if (this.bomb[y * this.width + x]) return 9;
+      getGameState() {
+        if (!this.dirty) return pstate;
 
-        let count = 0;
-        for (let dy = -1; dy < 2; dy++) {
-          for (let dx = -1; dx < 2; dx++) {
-            if (!dx && !dy) continue;
-            const nx = x + dx, ny = y + dy;
-            if (is_range(nx, ny, this.width, this.height)) {
-              if (this.bomb[ny * this.width + nx]) count++;
-            }
-          }
+        if (this.n < 0 || this.w * this.h < this.n) return GAME_IS_ERROR;
+
+        let f = false;
+        for (let pos = 0; pos < this.w * this.h; pos++) {
+          if (this.opened[pos] && this.bomb[pos]) return GAME_IS_OVER;
+          if (!this.opened[pos] && !this.bomb[pos]) f = true;
         }
-        return count;
+        return f ? GAME_IS_STILL_CONTINUED : GAME_IS_CLEAR;
       },
 
       open(x, y) {
-        this.opened.splice(y * this.height + x, 1, true);
-        if (this.getState(x, y) != 0) return; 
+        this.dirty = true;
+
+        this.opened.splice(y * this.w + x, 1, true);
+        if (this.cells[y * this.w + x] != 0) return;
+
         for (let dy = -1; dy < 2; dy++) {
           for (let dx = -1; dx < 2; dx++) {
             if (!dx && !dy) continue;
             const nx = x + dx, ny = y + dy;
-            if (is_range(nx, ny, this.width, this.height)) {
-              if (!this.opened[ny * this.width + nx]) this.open(nx, ny);
+            if (is_range(nx, ny, this.w, this.h)) {
+              if (!this.opened[coord2pos(nx, ny, this.w, this.h)]) this.open(nx, ny);
             }
           }
         }
       }
     },
+
     data() {
-      let bomb = [], opened = [];
-      for (let pos = 0; pos < this.width * this.height; pos++) {
+      let bomb = [], opened = [], cells = [];
+
+      for (let pos = 0; pos < this.w * this.h; pos++) {
         opened[pos] = false;
-        bomb[pos] = pos < this.bnum;
+        bomb[pos] = pos < this.n;
       }
+
+      bomb = shuffleArray(bomb);
+
+      for (let y = 0; y < this.h; y++) {
+        for (let x = 0; x < this.w; x++) {
+          let count = 0;
+          for (let dy = -1; dy < 2; dy++) {
+            for (let dx = -1; dx < 2; dx++) {
+              if (!dx && !dy) continue;
+              const nx = x + dx, ny = y + dy;
+              if (is_range(nx, ny, this.w, this.h) && bomb[coord2pos(nx, ny, this.w, this.h)])
+                count++;
+            }
+          }
+
+          const pos = coord2pos(x, y, this.w, this.h);
+          cells[pos] = bomb[pos] ? 9 : count;
+        }
+      }
+
       return {
-        bomb: shuffleArray(bomb),
-        opened: opened
+        bomb: bomb,
+        opened: opened,
+        cells: cells,
+        pstate: GAME_IS_STILL_CONTINUED,
+        dirty: true
       }
     }
   }
@@ -90,5 +150,26 @@
 <style>
   #app {
     background: #BDBDBD;
+  }
+  
+  .vail-rect {
+    fill: #FFFFFF;
+    fill-opacity: 0.5;
+  }
+
+  .result {
+    font-size: 20px;
+  }
+
+  .result-clear {
+
+  }
+
+  .result-over {
+
+  }
+
+  .result-error {
+
   }
 </style>
--- javascript/src/main.js
+++ javascript/src/main.js
@@ -4,10 +4,44 @@ import Vue from 'vue'
 import App from './App'
 
 Vue.config.productionTip = false
+Vue.config.performance = true;
+
+// ref: https://stackoverflow.com/questions/610406/javascript-equivalent-to-printf-string-format
+// First, checks if it isn't implemented yet.
+if (!String.prototype.format) {
+  String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) { 
+      return typeof args[number] != 'undefined'
+        ? args[number]
+        : match
+      ;
+    });
+  };
+}
+
+function getParams() {
+  let obj = {};
+  const h = document.location.href;
+  for (let v of h.substr(h.search(/\?/) + 1).split('&')) {
+    const key = v.substr(0, v.search('='));
+    const val = v.substr(v.search('=') + 1);
+    obj[key] = val;
+  }
+  return obj;
+}
+
+function test() {
+  const params = getParams();
+  const width = params['w'] || 8;
+  const height = params['h'] || 8;
+  const bnum = params['n'] || 8;
+  return '<App w="{0}" h="{1}" n="{2}" />'.format(width, height, bnum);
+}
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   components: {App},
-  template: '<App width="10" height="10" bnum="10" />'
-})
+  template: test()
+});
